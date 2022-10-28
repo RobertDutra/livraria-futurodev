a12b58a8c67d20df8806425af828c02290874e7e
Robert Dutra <robertdutra10@hotmail.com>
2022-10-28 19:43:59 -0300

    first commit

---
+++ src/main/java/com/br/livrariafuturodev/LivrariaFuturodevApplication.java
@@ -0,0 +1,13 @@
+package com.br.livrariafuturodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaFuturodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaFuturodevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/livrariafuturodev/LivrariaFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.livrariafuturodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
